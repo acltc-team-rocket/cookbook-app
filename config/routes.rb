5741096cc7d52a2a9fc00874fecbee095fd3d414
Mark Richardson <mark@markeban.com>
2016-08-01 19:29:40 -0700

    add sign up for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get '/', to: 'recipes#index'
   get '/recipes', to: 'recipes#index'
   get '/recipes/new', to: 'recipes#new'
   get '/recipes/:id', to: 'recipes#show'
   post '/recipes', to: 'recipes#create'
   get '/recipes/:id/edit', to: 'recipes#edit'
   patch '/recipes/:id', to: 'recipes#update'
+
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
   
 end
